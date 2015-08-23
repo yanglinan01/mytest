ab571bca78b2e3f80cb4d0c5785f80d5151ceb62
504727228 <504727228@qq.com>
2015-08-24 01:34:41 +0800

    111

--- src/com/yang/test/Test2.java
+++ src/com/yang/test/Test2.java
@@ -3,6 +3,7 @@ package com.yang.test;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("1111111111111111");
+        System.out.println("22222222222222222");
     }
 
 }
