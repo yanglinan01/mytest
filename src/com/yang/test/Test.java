40eacde49cfe813c1f14bdb91e221d82ff846d17
504727228 <504727228@qq.com>
2015-08-24 00:34:26 +0800

    1111

--- src/com/yang/test/Test.java
+++ src/com/yang/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("yang lin an 01");
         System.out.println("yang lin an 021");
+        System.out.println("yang lin an 0333");
     }
 
 }
